88cc64d72d86f8bd2e6f00162c2a1af46bc9dccb
Georgii Sergeev <g.sergeev@big3.ru>
2024-01-11 16:01:59 +0300

    second test added

--- test_google.py
+++ test_google.py
@@ -11,3 +11,9 @@ def test_selene(settings):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_empty_google(settings):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('usidhuadsjfnasoiphdvpoas').press_enter()
+    browser.element('[id="botstuff"]').should(have.text('ничего не найдено'))
