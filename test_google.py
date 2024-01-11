f741cdc3fe073a4421f5de39f84c6628f9672735
Georgii Sergeev <g.sergeev@big3.ru>
2024-01-11 15:40:45 +0300

    first test file update

--- test_google.py
+++ test_google.py
@@ -1,7 +1,13 @@
+import pytest
 from selene import browser, be, have
 
 
-def test_selene():
+@pytest.fixture
+def settings():
+    browser.driver.set_window_size(500, 500)
+
+
+def test_selene(settings):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
